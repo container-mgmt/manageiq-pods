9316126fa93f83d9a39c5ac8c46c37771937337c
Scott Weiss <sdw35@cornell.edu>
2017-08-30 14:16:02 +0300

    update ansible command

--- hack/ansible-command.sh
+++ hack/ansible-command.sh
@@ -8,8 +8,12 @@ ansible-playbook -i hosts -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
 
 
 ansible-playbook -i hosts -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
-    oo_first_master=[ocp-master01.10.35.48.199.nip.io] \
+    # oo_first_master=[] \
     prometheus_route=prometheus-cfme.10.35.48.200.nip.io \
     httpd_route=httpd-cfme.10.35.48.200.nip.io \
     alerts_route=alerts-cfme.10.35.48.200.nip.io" \
     ./miqplaybook.yml
+
+ansible-playbook -i hosts -vvv --extra-vars "openshift_prometheus_namespace=${MIQPROJECT} \
+    openshift_prometheus_additional_rules_file=${PWD}/prometheus-alert-rules.yml" \
+    ~/workspace/openshift-ansible/playbooks/byo/openshift-cluster/openshift-prometheus.yml
