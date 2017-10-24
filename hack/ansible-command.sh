786c60127d4ddacdda8925d9e16231ee0e35ba7e
Scott Weiss <sdw35@cornell.edu>
2017-10-24 12:04:35 -0400

    update ansible command

--- hack/ansible-command.sh
+++ hack/ansible-command.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
+export OC_TOKEN=
+export OO_FIRST_MASTER=
+export HTTPD_ROUTE=
+export PROMETHEUS_ROUTE=
+export ALERTS_ROUTE=
+
 source ${HOME}/workspace/ansible/hacking/env-setup
+
+#miq pods
+ansible-playbook -i hosts-ansible-miq.ini -vvv ~/workspace/openshift-ansible/playbooks/byo/openshift-management/config.yml
+
+#prometheus
+ansible-playbook -i /home/ilackarms/workspace/manageiq/manageiq-pods/hack/hosts-ansible-miq.ini \
+    -vvv --extra-vars "openshift_prometheus_namespace=prometheus     \
+    openshift_prometheus_additional_rules_file=/home/ilackarms/workspace/manageiq/manageiq-pods/hack/prometheus-alert-rules.yml" \
+    /home/ilackarms/workspace/openshift-ansible/playbooks/byo/openshift-cluster/openshift-prometheus.yml
+
+#set up provider
+ansible-playbook -i hosts-ansible-miq.ini -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
+    oo_first_master=ocp-master01.10.35.48.94.nip.io \
+    httpd_route=httpd-openshift-management.10.35.48.95.nip.io \
+    prometheus_route=prometheus-cfme.10.35.48.95.nip.io \
+    alerts_route=alerts-cfme.10.35.48.95.nip.io" \
+    ./miqplaybook.yml
 
 #stable
 ansible-playbook -i hosts.ini -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
@@ -14,7 +37,7 @@ ansible-playbook -i hosts-stable.ini -vvv --extra-vars "openshift_prometheus_nam
     openshift_prometheus_additional_rules_file=${PWD}/prometheus-alert-rules.yml" \
     ~/workspace/openshift-ansible/playbooks/byo/openshift-cluster/openshift-prometheus.yml
 
-#unstable
+#unstable - old
 ansible-playbook -i hosts.ini -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
     oo_first_master=ocp-master01.10.35.48.79.nip.io \
     httpd_route=httpd-cfme.10.35.48.80.nip.io \
@@ -22,6 +45,21 @@ ansible-playbook -i hosts.ini -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN}
     alerts_route=alerts-cfme.10.35.48.80.nip.io" \
     ./miqplaybook.yml
 
+#unstable - external db
+ansible-playbook -i hosts.ini -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
+    oo_first_master=ocp-master01.10.35.48.79.nip.io \
+    httpd_route=httpd-openshift-management.10.35.48.80.nip.io \
+    prometheus_route=prometheus-cfme.10.35.48.80.nip.io \
+    alerts_route=alerts-cfme.10.35.48.80.nip.io" \
+    ./miqplaybook.yml
+
 ansible-playbook -i hosts-unstable.ini -vvv --extra-vars "openshift_prometheus_namespace=cfme \
     openshift_prometheus_additional_rules_file=${PWD}/prometheus-alert-rules.yml" \
     ~/workspace/openshift-ansible/playbooks/byo/openshift-cluster/openshift-prometheus.yml
+
+ansible-playbook -i hosts-ansible-miq.ini -vvv --extra-vars "mgmt_infra_sa_token=${OC_TOKEN} \
+    oo_first_master=ocp-master01.10.35.48.94.nip.io \
+    httpd_route=httpd-openshift-management.10.35.48.95.nip.io \
+    prometheus_route=prometheus-cfme.10.35.48.95.nip.io \
+    alerts_route=alerts-cfme.10.35.48.95.nip.io" \
+    ./miqplaybook.yml
